27863eecae144ec4d76f319d6f9c8ecf5186176b
Lim Guowei <iamapokemonmaster@gmail.com>
2025-05-12 20:12:42 +0800

    Rework Goal model to prepare for django-simple-history

--- be_django/be/admin.py
+++ be_django/be/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+from .models import User, Goal, Project, Task
+
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+  pass
+
+class GoalAdmin(admin.ModelAdmin):
+  pass
+
+# class GoalHistoryAdmin(admin.ModelAdmin):
+#   pass
+
+class ProjectAdmin(admin.ModelAdmin):
+  pass
+
+class TaskAdmin(admin.ModelAdmin):
+  pass
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Goal, GoalAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Task, TaskAdmin)
--- be_django/be/models.py
+++ be_django/be/models.py
@@ -10,26 +10,50 @@ class User(models.Model):
   created_at = models.DateTimeField(auto_now=True)
 
   def __str__(self):
-    return f"{self.first_name} {self.last_name} - {self.email}"
+    return f"{self.first_name} {self.last_name}"
+
+
+# class Goal(models.Model):
+#   user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+#   current_version = models.ForeignKey('GoalHistory', on_delete=models.SET_NULL)
+
+  # def save_new_version(self, title, description, priority, due_date):
+  #   version_number = GoalHistory.objects.filter(goal=self).count()+1
+  #   # Create a GoalHistory object and save the latest entry in the table
+  #   history = GoalHistory.objects.create(
+  #     goal=self,
+  #     title=title,
+  #     description=description,
+  #     priority=priority,
+  #     due_date=due_date,
+  #     version_number=version_number
+  #   )
+  #   self.current_version = history
+  #   self.save()
+
 
 class Goal(models.Model):
-  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-
-class GoalHistory(models.Model):
-  goal_id = models.ForeignKey(Goal, on_delete=models.CASCADE)
+  # goal_id = models.ForeignKey(Goal, on_delete=models.CASCADE)
   goal_name = models.CharField(max_length=140)
-  goal_description = models.CharField(max_length=2000)
+  goal_description = models.TextField(null=True, blank=True)
   priority = models.IntegerField(
+    default=1,
     validators=[MinValueValidator(1), MaxValueValidator(10)]
   )
-  due_date = models.DateTimeField()
+  due_date = models.DateTimeField(null=True, blank=True)
   created_at = models.DateTimeField(auto_now=True)
+  # version_number= models.IntegerField()
+
+  # class Meta:
+  #   verbose_name_plural = "Goal Histories"
+
 
 class Project(models.Model):
   project_name = models.CharField(max_length=140)
-  project_description = models.CharField(max_length=2000)
+  project_description = models.TextField()
   parent_id = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
   user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 class Task(models.Model):
   TASK_TYPE_CHOICES = {
@@ -39,7 +63,7 @@ class Task(models.Model):
   }
   task_name = models.CharField(max_length=140)
   task_type = models.CharField(max_length=5, choices=TASK_TYPE_CHOICES)
-  task_description = models.CharField(max_length=2000)
+  task_description = models.TextField()
   parent_id = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
   project_id = models.ForeignKey(Project, null=True, blank=True, on_delete=models.CASCADE)
   user_id = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -49,6 +73,9 @@ class Task(models.Model):
     validators=[MinValueValidator(1), MaxValueValidator(10)]
   )
   is_completed = models.BooleanField()
-  recurrence_interval = models.DurationField()
+  recurrence_interval = models.DurationField(null=True, blank=True)
+
+  def __str__(self):
+    return self.task_name
 
   
